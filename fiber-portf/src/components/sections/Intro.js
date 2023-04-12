7eed739b4ca5bde26898a809cae2df49006477d9
danyatcode <danyatd22@gmail.com>
2023-04-12 14:34:10 +0300

    Second version of pageart

--- fiber-portf/src/components/sections/Intro.js
+++ fiber-portf/src/components/sections/Intro.js
@@ -39,7 +39,7 @@ const Intro = () => {
         translateY={[-30, 30]}
         className='relative max-h-auto max-w-full inset-0 h-full w-full object-cover z-0'
         >           
-      <img loading='lazy' src={process.env.PUBLIC_URL + '/images/Page_arts.webp'} className='intro-image absolute max-h-auto max-w-full inset-0 h-full w-full object-cover' alt='img'></img>
+      <img src={process.env.PUBLIC_URL + `/images/${isMobile? "Page_arts_mobile.webp":"Page_arts.webp"}`} className='intro-image absolute max-h-auto max-w-full inset-0 h-full w-full object-cover' alt='img'></img>
       <div className='intro-inner flex flex-col align-middle gap-x-5 mx-20 relative z-10'>
         <div className='rose'>
             <img src={process.env.PUBLIC_URL + "/images/Rose_logo.webp"} className='object-cover max-h-auto max-w-full' alt='rose'></img>
